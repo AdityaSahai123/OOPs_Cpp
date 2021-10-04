1461f69bd329dc413a9c5b325c17114e803bbad4
AdityaSahai123 <72221032+AdityaSahai123@users.noreply.github.com>
2021-10-04 15:56:25 +0530

    Add files via upload

---
+++ Practical-03/Practical-03_Task3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cin>>a;
+    cout<<a;
+
+    return 1;
+}
+
---
+++ Practical-03/Practical-03_Task4.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    string c;
+    getline(cin, c);
+    cout << c;
+    return 1;
+
+}
